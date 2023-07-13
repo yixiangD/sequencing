7ade23d1e0b3f666d8dea021604506e698e90351
yixiangD <yxdeng05@gmail.com>
2023-07-13 18:35:50 -0400

    update with a small dataset

--- example.sh
+++ example.sh
@@ -1,6 +1,6 @@
 fd_name="data/all_data"
-out="test2"
+out="LIB058683_GEN00257688_M4-CD21lo-HC_S16"
 mixcr analyze rnaseq-bcr-full-length --species mouse \
-$fd_name/LIB058683_GEN00257658_day0-HC_S1_L001_R1_001.fastq.gz \
-$fd_name/LIB058683_GEN00257658_day0-HC_S1_L001_R2_001.fastq.gz \
-result/$out
+    $fd_name/${out}_L001_R1_001.fastq.gz \
+    $fd_name/${out}_L001_R2_001.fastq.gz \
+    result/$out
