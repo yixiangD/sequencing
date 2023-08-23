447918bcf60f840c240af4071d7555394362deeb
yixiangD <yxdeng05@gmail.com>
2023-08-23 00:43:22 -0400

    batch sub job

--- myjob.sh
+++ myjob.sh
@@ -3,15 +3,17 @@
 #SBATCH -o job_output/test-%J.out
 #SBATCH -e job_output/test-%J.err
 #SBATCH --nodes 1
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem-per-cpu=16G
 
 # Some important variables to check (Can be removed later)
 module load java/14.0.1
 # Add your job command here
 fd_name="data/all_data"
-out="LIB058683_GEN00257688_M4-CD21lo-HC_S16"
+#out="LIB058683_GEN00257688_M4-CD21lo-HC_S16"
+while read out; do
 mixcr analyze rnaseq-bcr-full-length --species mouse \
     $fd_name/${out}_L001_R1_001.fastq.gz \
     $fd_name/${out}_L001_R2_001.fastq.gz \
-    result/$out
+    result/${out}
+done < all_but_one_data.txt
