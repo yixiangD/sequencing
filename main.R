4284f6ce2c1ddefe029e8d454a4e5f25a3fa75e9
yixiangD <yxdeng05@gmail.com>
2023-09-04 23:32:19 -0400

    output results

--- main.R
+++ main.R
@@ -3,8 +3,31 @@ data <- read.table(file,              # TXT data file indicated as string or ful
            header = FALSE,    # Whether to display the header (TRUE) or not (FALSE)
            sep = "\n")         # Character used to separate decimals of the numbers in the file
 path <- "/Users/yd973/Dropbox (Partners HealthCare)/danni/results"
-for (i in seq(1, dim(data)[1])) {
-    fname <- paste0(path, "/", data[i, 1], ".clones_IGH.tsv")
+n.arr <- seq(1, dim(data)[1])
+# n.arr <- c(1, 2, 3)
+res.final <- c()
+for (i in n.arr) {
+    label <- data[i, 1]
+    code <- strsplit(label, "_")[[1]][3]
+    mice <- strsplit(code, "-")[[1]][1]
+    pop <- strsplit(code, "-")[[1]][2]
+
+    fname <- paste0(path, "/", label, ".clones_IGH.tsv")
     df <- as.data.frame(data.table::fread(fname))
-    cols <- columns(df)
+    # cols <- colnames(df)
+    sel.col <- "allCHitsWithScore"
+    arr <- df[[sel.col]]
+    arr.new <- unlist(stringr::str_split(arr,","))
+    # length(arr.new) > length(arr)
+    arr.ig <- sub("\\*.*", "", arr.new)
+    res <- data.frame(table(arr.ig))
+    res <- res[2:dim(res)[1], ]
+    res$mice <- mice
+    res$pop <- pop
+    colnames(res)[1] <- "ig"
+    row.names(res) <- NULL
+    res.final <- rbind(res.final, res)
 }
+# print(res.final)
+outpath <- "./result/seq_res.xlsx"
+writexl::write_xlsx(res.final, outpath)
